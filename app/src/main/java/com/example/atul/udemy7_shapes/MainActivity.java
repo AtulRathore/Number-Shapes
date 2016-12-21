31f70892092d8fc38b0e45a390a7bd68a0d5290c
Atul Rathore <kshtriya.atul@gmail.com>
2016-12-21 23:41:36 +0530

    First Commit with classes.

--- app/src/main/java/com/example/atul/udemy7_shapes/MainActivity.java
+++ app/src/main/java/com/example/atul/udemy7_shapes/MainActivity.java
@@ -5,21 +5,78 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    class Shapes{
+        int number;
+
+        public boolean isTriangular(){
+            int x = 1;
+            int triangularNumber = 1;
+            while(triangularNumber < number){
+                x++;
+                triangularNumber = triangularNumber + x;
+            }
+
+            if(number==triangularNumber){
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        public boolean isSquare(){
+            double squareRoot = Math.sqrt(number);
+
+            if(squareRoot==Math.floor(squareRoot)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
 
 
 
     public void testNumber(View view){
 
+        String message ="";
         EditText userNumber = (EditText) findViewById(R.id.userNumber);
         Log.i("User Number", userNumber.getText().toString());
 
-        /*Shapes myNUm = new Shapes();
-        myNUm.number = 7;
+        if(userNumber.getText().toString().isEmpty()){
+            message="Please enter a number.";
+        }
+        else {
 
-        System.out.println(myNUm.isSquare());*/
+        Shapes myNUm = new Shapes();
+
+        myNUm.number = Integer.parseInt(userNumber.getText().toString());
+
+        if(myNUm.isSquare()){
+
+            if(myNUm.isTriangular()){
+                message= myNUm.number+" is both Triangular and Square";
+            }else{
+                message=myNUm.number+" is only Square but not Triangular";
+            }
+        }else {
+            if(myNUm.isTriangular()){
+                message= myNUm.number+" is Triangular but not Square";
+            }else{
+                message=myNUm.number+" is neither Triangular not Square ";
+            }
+            //message=myNUm.number+" is only a Triangular";
+        }
+
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+       // System.out.println(myNUm.isSquare());
 
     }
 
